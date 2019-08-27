a62943e05b7a7746bce0e26012eb042c6259b869
Jessica Chin <jntchin@gmail.com>
2019-08-27 17:18:39 -0400

    first commit

---
+++ scripts/pseudocode.js
@@ -0,0 +1,14 @@
+// recipeGenApp
+
+// user selects main ingredient from drop-down menu
+// based on user selection, go through the array of recipes on mealdb.com and return all the objects (recipes) that contain selected main ingredient in the object's ingredient key to an array mainIngredientRecipes
+// user then selects from a drop-down list of allergens
+// go through each recipe object in mainIngredientRecipes and return all objects that DO NOT contain selected allergen in ingredient key to a new array filteredRecipes.
+// append some of the key values (recipe name and image) from filteredRecipes to an html element
+// when user clicks on html element, it expands to reveal entire recipe
+
+
+// STRETCH GOALS
+
+//multiple allergen filters
+//animation (animated header, clicking on a recipe prints the information on a recipe card)
