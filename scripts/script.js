eba703a0fcbfcab594df22cea1bf58cc938b2223
Jessica Chin <jntchin@gmail.com>
2019-08-28 16:19:59 -0400

    html done?

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+//async await
+//API call search by category
+// this returns an array containing all of the recipe IDs in the category chosen by the user
+//2nd API call using the returned IDs as the query parameter to return full recipes for each ID
+//  this returns an array of objects, and each object has all the information about the recipe
+// create an array based on the allergens checked off by the user
+// run a function that checks if any strIngredient keys include the allergens selected by the user
+// if so, remove from the array
+// display the final array to the user
+
+
+//don't forget the close button on the final recipe card
