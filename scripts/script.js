3a1b687cb7d5d2333d460f3e544f83ac0db957da
Jessica Chin <jntchin@gmail.com>
2019-08-28 16:55:14 -0400

    started writing functions

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,44 @@
 
 
 //don't forget the close button on the final recipe card
+
+const recipeGenApp = {};
+const apiUrlFilterByMainIngredient = 'https://www.themealdb.com/api/json/v1/1/filter.php'; 
+const apiUrlReturnRecipesByID = 'https://www.themealdb.com/api/json/v1/1/lookup.php';
+
+
+recipeGenApp.categoriesQuery = function(){
+    return $.ajax({
+        url: `${apiUrlFilterByMainIngredient}c=${userMainIngredient}`,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            key: 1,
+            
+        }
+    });
+};
+
+
+recipeGenApp.idQuery = function(){
+    return $.ajax({
+        url: `${apiUrlReturnRecipesByID}`,
+        //write a function that generates these URLs^
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            key: 1,
+        }
+    })
+}
+
+
+
+
+
+//TESTING:
+// const allergens = [`peas`, `tomato`];
+// const ingredients = "peasfishpeanutscornflourwater";
+
+// const result = ingredients.includes(...allergens);
+// console.log(result); //This returns true because it includes at least one of the allergens
