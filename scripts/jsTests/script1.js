6432b2fe8886d33be890084ae7815fb77c3cabc6
Jessica Chin <jntchin@gmail.com>
2019-09-03 00:23:35 -0400

    design changes for full screen done, still need to make responsive

--- scripts/jsTests/script1.js
+++ scripts/jsTests/script1.js
@@ -3,9 +3,12 @@ const recipeGenApp = {};
 const apiUrlFilterByMainIngredient = 'https://www.themealdb.com/api/json/v1/1/filter.php'; 
 const apiUrlReturnRecipesByID = 'https://www.themealdb.com/api/json/v1/1/lookup.php';
 
+​
+
 recipeGenApp.finalRecipes = [];
 recipeGenApp.finalIngredients = [];
 recipeGenApp.finalMeasurements = [];
+
 
 recipeGenApp.formSubmit = function(){
     $('form').on('submit', function(e){
@@ -15,14 +18,16 @@ recipeGenApp.formSubmit = function(){
     })
 }
 
+​
+
 //main app that generates content for page based on user search queries:
 
 recipeGenApp.categoriesQuery = function(){
-    //hide the button that allows users to search for recipes
+   //hide the button that allows users to search for recipes
     $('button.generateRecipes').addClass('hidden');
     //variable to store selected main ingredient
     recipeGenApp.userMainIngredient = $('option:selected').attr('value');
-     //variable to store selected allergen
+    //variable to store selected allergen
     recipeGenApp.userAllergen = $("input[name='allergen']:checked").attr('value');
     //first API call to return recipe IDs
     $.ajax({
@@ -32,9 +37,9 @@ recipeGenApp.categoriesQuery = function(){
         data: {
             key: 1
         },
-    //once recipe IDs are returned
+     //once recipe IDs are returned
     }).then(res => {
-         //map over the returned results to extract the IDs and store them in idArr
+        //map over the returned results to extract the IDs and store them in idArr
         let idArr = res.meals.map(meal => meal.idMeal);
         //map over idArr and for each ID, use it for an Ajax request that will a promise.  Store all the promises in promisesArray
         const promisesArray = idArr.map(el =>{
@@ -48,40 +53,42 @@ recipeGenApp.categoriesQuery = function(){
                         }
             })
         })
-    //when all promises have been returned, gather them into an array...
+
+        //when all promises have been returned, gather them into an array...
         $.when(...promisesArray)
         //...and unpack the array into individual elements
             .then((...res) => {
-                 //empty the finalRecipes array
+                //empty the finalRecipes array
                 recipeGenApp.finalRecipes.length = 0 ;
-                 //for each recipe object returned, check if its recipe instructions include the user allergen.  If NOT, push the object into the finalRecipes array 
+                //for each recipe object returned, check if its recipe instructions include the user allergen.  If NOT, push the object into the finalRecipes array 
                 res.forEach(function(item){
                     if (!item[0].meals[0].strInstructions.includes(recipeGenApp.userAllergen)){
                         recipeGenApp.finalRecipes.push(item[0].meals[0]);
                     }
                 });
 
+
                 let count = 0;
 
-                 //for each item in the recipe, 
+                //for each item in the recipe, 
 
-                recipeGenApp.finalRecipes.forEach(function(recipe){   
+                recipeGenApp.finalRecipes.forEach(function(recipe){  
                     //add 1 to the counter  
                     count = count + 1;
-                     //and go through the 20 strIngredients and push whichever ones are not null into the finalIngredients array
+                    //and go through the 20 strIngredients and push whichever ones are not null into the finalIngredients array
                     for(i=1;i<21;i++){
                        if (recipe[`strIngredient${i}`] !== null){
                            recipeGenApp.finalIngredients.push(recipe[`strIngredient${i}`]);
                        }
                     }
-                     //do the same thing for the strMeasurements and store whichever ones are not null in the finalMeasurements array
-
+                    //do the same thing for the strMeasurements and store whichever ones are not null in the finalMeasurements array
                     for(i=1;i<21;i++){
                        if (recipe[`strMeasure${i}`] !== null){
                            recipeGenApp.finalMeasurements.push(recipe[`strMeasure${i}`]);
                        }
                     }
-                     //append the following HTML to the ul.recipePreviews section:
+
+                    //append the following HTML to the ul.recipePreviews section:
                     $('ul.recipePreviews').append(`
                     <li class="recipe${count} fullRecipe">
                             <div class="container">
@@ -104,23 +111,24 @@ recipeGenApp.categoriesQuery = function(){
                             </div>
                     </li>
                     `);
+
                     //append each ingredient in finalIngredients to li.recipe1, li.recipe2, etc.
                     recipeGenApp.finalIngredients.forEach((ing)=> {
                         $(`li.recipe${count} ul.ingredients`).append(`
                             <li>${ing}</li>
                         `) 
                     });
+
                     //append each measurement in finalMeasurements to li.rec1, li.recipe2, etc.
                     recipeGenApp.finalMeasurements.forEach((mes)=> {
                         $(`li.recipe${count} ul.measurements`).append(`<li>${mes}</li>`) 
                     });
+
                     //empty the finalIngredients array and the finalMeasurements array
                     (recipeGenApp.finalIngredients).length = 0;
                     (recipeGenApp.finalMeasurements).length = 0;
                 })
-                
             }); 
-
     });
 };
 
@@ -146,14 +154,18 @@ recipeGenApp.hideRecipe = function(){
     })
 };
 
+​
 
 //app init
+
 recipeGenApp.init = function(){
     recipeGenApp.formSubmit();
     recipeGenApp.showRecipe();
     recipeGenApp.hideRecipe();
 }
 
+
 $(document).ready(function(){
     recipeGenApp.init();
 });
+
