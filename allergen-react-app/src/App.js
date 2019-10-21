b21f26cfcd05ae82c05757b6ae484b8a4dff41e8
Jessica Chin <jntchin@gmail.com>
2019-10-21 15:18:17 -0400

    writing axios calls

--- allergen-react-app/src/App.js
+++ allergen-react-app/src/App.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state ={
+
+    }
+  }
+
+  //axios call
+  filterByMainIngredient = () =>{
+    axios({
+      method: 'GET',
+      url: 'https://www.themealdb.com/api/json/v1/1/filter.php',
+      dataResponse: 'JSON',
+      data: {
+        key: 1
+      },
+    })
+  }
+
+  getRecipesById = () =>{
+    axios({
+      method: 'GET',
+      url: 'https://www.themealdb.com/api/json/v1/1/lookup.php',
+      dataResponse: 'JSON',
+      data: {
+        key: 1
+      },
+      
+    })
+  }
+
   render(){
     return (
       <div className="App">
