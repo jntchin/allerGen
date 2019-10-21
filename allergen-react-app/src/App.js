c7ea173aae2fddd74a5014dae7d02e9cef02ef79
Jessica Chin <jntchin@gmail.com>
2019-10-21 14:46:09 -0400

    adding react

---
+++ allergen-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Aller-gen</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
